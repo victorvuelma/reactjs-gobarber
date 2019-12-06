2becf207cc0c7fe528875116ac7b015b0e6808fe
Victor Vuelma <victorvuelma@outlook.com>
2019-12-06 17:48:46 -0300

    configure private routes

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,6 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'no-console': ['warn', { allow: ['tron'] }],
+    'react/jsx-props-no-spreading': 'off',
   },
 };
