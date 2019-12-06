101445ce6bcc4b62e179d73cb741beba719742a3
Victor Vuelma <victorvuelma@outlook.com>
2019-12-06 18:24:17 -0300

    convert import to root import

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,8 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 export default function Routes() {
   return (
