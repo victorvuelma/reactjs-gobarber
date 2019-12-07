3f68cfb4b26ee7afb56fe4389102d6f62e0a79f5
Victor Vuelma <victorvuelma@outlook.com>
2019-12-07 17:21:35 -0300

    create dashboard style

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
 
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>07/12/2019</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Victor Vuelma</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Victor Vuelma</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Victor Vuelma</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
