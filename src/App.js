c02ebd2e73bcbd4602721e7a8b83c7933bc89cdf
Victor Vuelma <victorvuelma@outlook.com>
2019-12-06 21:20:14 -0300

    setup redux persist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 
-import store from './store';
+import { store, persistor } from './store';
 
 import Routes from './routes';
 import history from './services/history';
@@ -14,10 +15,12 @@ import GlobalStyle from './styles/global';
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
