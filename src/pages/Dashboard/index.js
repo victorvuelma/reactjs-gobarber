486890369bbe87694088b44f5a67d90d4c3edcb0
Victor Vuelma <victorvuelma@outlook.com>
2019-12-07 17:56:27 -0300

    show appointments from api

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,18 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  setMilliseconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
+import { zonedTimeToUtc } from 'date-fns-tz';
+import pt from 'date-fns/locale/pt-BR';
 
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
@@ -6,36 +20,76 @@ import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+  const [schedule, setSchedule] = useState([]);
+
+  const dateFormated = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule', {
+        params: { date },
+      });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkDate = setMilliseconds(
+          setSeconds(setMinutes(setHours(date, hour), 0), 0),
+          0
+        );
+        const compareDate = zonedTimeToUtc(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(a => {
+            return isEqual(parseISO(a.date), compareDate);
+          }),
+        };
+      });
+
+      setSchedule(data);
+    }
+
+    loadSchedule();
+  }, [date]);
+
+  function handlePrevDate() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDate() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button onClick={handlePrevDate} type="button">
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>07/12/2019</strong>
-        <button type="button">
+        <strong>{dateFormated}</strong>
+        <button onClick={handleNextDate} type="button">
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
 
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Victor Vuelma</span>
-        </Time>
-        <Time available>
-          <strong>09:00</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time>
-          <strong>10:00</strong>
-          <span>Victor Vuelma</span>
-        </Time>
-        <Time>
-          <strong>11:00</strong>
-          <span>Victor Vuelma</span>
-        </Time>
+        {schedule.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Em aberto'}
+            </span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
